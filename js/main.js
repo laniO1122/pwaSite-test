8e8d774ff8d707b648b6746be97364cfe691cf98
laniO1122 <55339555+laniO1122@users.noreply.github.com>
2021-04-16 17:10:20 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//make sure sw are supported
+if('serviceWorker' in navigator){
+    //console.log('Service Worker Supported');
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+        .register('../sw_cached_pages.js')
+        .then(reg => console.log('Service Worker: Registered'))
+        .catch(err => console.log(`Service Worker: Error: ${err}`))
+    })
+}
---
+++ sw_cached_site.js
@@ -0,0 +1,56 @@
+// Cache the whole site
+const cacheName = 'v2';
+
+
+//call install event
+self.addEventListener('install', e => {
+    console.log('Service Worker: Installed');
+});
+
+//Call activate event
+self.addEventListener('activate', e => {
+    console.log('Service Worker: Activated');
+    //Remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if(cache !== cacheName) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+//call Fetch Event
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+        fetch(e.request)
+        .then(res => {
+            //Make copy/clone of response
+            const resClone = res.clone();
+            //Open cache
+            caches
+            .open(cacheName)
+            .then(cache => {
+                // Add response to cache
+                cache.put(e.request, resClone);
+            });
+        return res;
+     }).catch(err => caches.match(e.request).then(res => res))
+    );
+});
+
+
+
+
+
+
+
+
+
+
